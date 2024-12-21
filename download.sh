bfc1996871ae01f459c5ccfd7d399559cd57fcfb
scarick <75560274+Kairui-SHI@users.noreply.github.com>
2024-12-21 22:25:16 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,2 +1,2 @@
 # download zip file from google drive
-curl --no-check-certificate https://drive.google.com/file/d/1x5FwBtA0CRn3MQ2WFQ8gk9iKhxWCb0X1/view?usp=sharing
+curl -L -o ppt_template1.zip https://drive.google.com/file/d/1x5FwBtA0CRn3MQ2WFQ8gk9iKhxWCb0X1/view?usp=sharing
